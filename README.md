859f61de023a8c51327bc12440abed810b6089a3
VictorNuzhdin <nuzhdin.vicx@yandex.ru>
2023-03-26 04:45:00 +0600

    Webapp ready

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # sf-task927-nodejs-app
 For Skill Factory study project (B9, Task B9.2.7)
+
+<br>
+
+### install dependencies without package.json
+
+```bash
+$ npm install jquery
+$ npm install jsdom
+```
+
+### install dependencies with package.json
+
+```bash
+$ npm install
+
+=OUTPUT:
+added 60 packages, and audited 61 packages in 7s
+found 0 vulnerabilities
+```
+
+### run
+
+```bash
+$ node index.js
+```
+
+### check result
+
+01 index.html страница приложения ДО изменения DOM с помощью jQuery
+![screen](_screens/01_index-page_before.png?raw=true)
+
+02 index.html страница приложения ПОСЛЕ изменения DOM с помощью jQuery
+![screen](_screens/02_index-page_after.png?raw=true)
---
+++ index.js
@@ -0,0 +1,53 @@
+const http = require("http");
+const fs = require("fs");
+
+//const { JSDOM } = require("jsdom");
+//const { window } = new JSDOM("");
+//const $ = require("jquery")(window);
+
+//https://github.com/jsdom/jsdom
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+
+const hostname = '0.0.0.0';
+const port = 8000;
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/html');
+
+    fs.readFile('./index.html', null, function (error, html_code) {
+        if (error) {
+            response.writeHead(404);
+            response.write('Sorry! File not found!');
+        } else {
+
+            // Create JSDOM "dom" object from html string data
+            //const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+            const dom = new JSDOM(html_code);
+            const $ = require("jquery")(dom.window);
+
+            // Get element by tag name
+            //console.log(dom.window.document.querySelector("p").textContent);    // test
+
+            // JSDOM selector test
+            dom.window.document.getElementById("message").textContent = "Holla";
+            dom.window.document.getElementById("name").textContent = "You";
+
+            // JQuery selector test
+            $("#jq_message").html("This text generated from Node " + process.version + " by JQuery");
+
+            // Return HTML page/code
+            response.write(html_code);            // Returns string of html code
+            //response.write(dom);                  // ERROR: Received an instance of JSDOM
+            //response.write(dom.serialize());        // FIX: Serializing dom-object to html-code
+        }
+        response.end();
+    }); //<--fs.readFile
+
+}); //<--server
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
